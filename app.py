042da448da699f2ae0af7c4072485cbea37eef77
PMGUMBLE <96189065+pmgumble@users.noreply.github.com>
2023-06-15 04:17:37 +0000

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 app = Flask(__name__)
@@ -17,6 +17,19 @@ def predict():
     prediction = clf.predict(tokenized_email)
     prediction = 1 if prediction == 1 else -1
     return render_template("index.html", prediction=prediction, email=email)
+
+
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    tokenized_email = cv.transform([email]) # X 
+    prediction = clf.predict(tokenized_email)
+    # If the email is spam prediction should be 1
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
